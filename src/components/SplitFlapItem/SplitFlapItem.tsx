d25c23a9799a5da6492f3dad74b2203582c29530
Joseph Riley <jriley@Josephs-MacBook-Air.local>
2020-04-14 12:10:00 -0400

    splits added; items updating; not yet flapping;

--- src/components/SplitFlapItem/SplitFlapItem.tsx
+++ src/components/SplitFlapItem/SplitFlapItem.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const ItemWrapper = styled.div`
   width: 100%;
   height: 100%;
-  padding: 2px;
+  padding: 1px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,10 +12,11 @@ const ItemWrapper = styled.div`
 
 const StyledItem = styled.div`
   width: 100%;
-  height: 100%;
+  height: 85%;
   line-height: 100$;
-  border: 1px solid #3d3d3d;
+  border: 1px solid #2d2d2d;
   color: #ede8d5;
+  background: #1d1d1d;
   text-transform: uppercase;
   font-size: 3.5rem;
   display: flex;
@@ -24,7 +25,7 @@ const StyledItem = styled.div`
   box-sizing: border-box;
   mouse-events: none;
   position: relative;
-  box-shadow: inset 4px 3px black;
+  box-shadow: inset 4px 3px 5px black;
   perspective: 500px;
 
   -webkit-touch-callout: none; /* iOS Safari */
@@ -39,21 +40,59 @@ const StyledItem = styled.div`
     position: absolute;
     top: 50%;
     left: 0;
-    border-top: 3px solid black;
+    border-top: 2px solid black;
+    box-shadow: 1px 1px 4px black;
     width: 100%;
     transform: translateY(-50%);
   }
+
+  .top {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    line-height: 100%;
+    height: 100%;
+    width: 100%;
+    background-color: #1d1d1d;
+    box-shadow: inset 4px 3px 5px black;
+    overflow: hidden;
+    z-index: 0;
+    clip-path: polygon(0 0, 100% 0, 100% 49%, 0 49%);
+  }
+
+  .bottom {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    line-height: 100%;
+    height: 100%;
+    width: 100%;
+    background-color: #1d1d1d;
+    box-shadow: inset 4px 3px 5px black;
+    overflow: hidden;
+    z-index: 0;
+    clip-path: polygon(0 51%, 100% 51%, 100% 100%, 0 100%);
+  }
+
 `;
 
 interface SFProps {
-    item: string
+  item: string,
+  nextItem: string,
 }
 
-const SplitFlapItem: React.FC<SFProps> = ({ item }) => {    
+const SplitFlapItem: React.FC<SFProps> = ({ item, nextItem }) => { 
     return (
       <ItemWrapper>
         <StyledItem>
-          <div>{item}</div>
+          <div className='top'>{item}</div>
+          <div className='bottom'>{item}</div>
         </StyledItem>
       </ItemWrapper>
     );
